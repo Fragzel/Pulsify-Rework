93374a581c83e3166479ddc842d2ae1cc68e381f
Khaled CHEHAB <lynckit@hotmail.com>
2024-11-13 03:59:14 +0100

    Fix typo in application title from "Pusify" to "Pulsify"

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -25,7 +25,7 @@ function App({ Component, pageProps }) {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <Head>
-          <title>Pusify</title>
+          <title>Pulsify</title>
         </Head>
         <Component {...pageProps} />
       </PersistGate>
